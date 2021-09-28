e283f5de23f7a44b2df38038eab234c1ea6e6ad4
Iván López <lopez.ivan@gmail.com>
2021-09-28 09:35:08 +0200

    Listen to retry events

--- src/main/groovy/micronaut3/test/TestHandler.groovy
+++ src/main/groovy/micronaut3/test/TestHandler.groovy
@@ -3,17 +3,24 @@ package micronaut3.test
 import groovy.util.logging.Slf4j
 import io.micronaut.retry.annotation.Recoverable
 import io.micronaut.retry.annotation.Retryable
+import io.micronaut.retry.event.RetryEvent
+import io.micronaut.retry.event.RetryEventListener
 import jakarta.inject.Singleton
 
 @Singleton
 @Slf4j
 @Recoverable(api = ITestHandler)
-class TestHandler implements ITestHandler {
+class TestHandler implements ITestHandler, RetryEventListener {
 
     @Override
     // Comment out this to see that the tests finishes soon because there is no retry or delay
     @Retryable(attempts = '2', delay = '1s', multiplier = '0')
     String handle() {
         throw new RuntimeException('test error')
+    }
+
+    @Override
+    void onApplicationEvent(RetryEvent event) {
+        println "============================ TestHandler.onApplicationEvent ============================"
     }
 }
